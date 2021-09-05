d0b62f4e776b56b39f6a8b07274d4da822f03c95
Evgeny_Bobkov <freeedayy@gmail.com>
2021-09-05 17:40:20 +1000

    v1.1 added new user form

--- src/components/fields.jsx
+++ src/components/fields.jsx
@@ -1,16 +1,67 @@
 import React, { useState } from "react";
 import Workers from "./worker";
 import * as api from "./FAKE_API/users.api";
+import {
+	Form,
+	Button,
+	Col,
+	InputGroup,
+	FormControl,
+	Row,
+	Spinner,
+} from "react-bootstrap";
 
 const Fields = () => {
-	const fields = api.fetchFieldsData();
 	const [selectedFieldData, setWorkers] = useState(0);
+	const [fields, setFields] = useState(api.fetchFieldsData());
+	const [newUserForm, setUserFormStatus] = useState(0);
 
 	const setSelectedField = (fieldNumber) => {
 		setWorkers(api.fetchFieldsData()[fieldNumber - 1]);
 	};
 
 	const reset = () => setWorkers(0);
+
+	const toggleUserFormActive = () => {
+		newUserForm ? setUserFormStatus(0) : setUserFormStatus(1);
+	};
+
+	const renderNewUserForm = () => (
+		<Form className="mt-3 " onSubmit={() => toggleUserFormActive()}>
+			<Row className="align-items-center">
+				<Col xs="auto">
+					<Form.Label htmlFor="inlineFormInput" visuallyHidden>
+						Name
+					</Form.Label>
+					<Form.Control
+						className="mb-2"
+						id="inlineFormInput"
+						placeholder="Ф.И.О Пользователя"
+					/>
+				</Col>
+				<Col xs="auto">
+					<Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
+						Position
+					</Form.Label>
+					<InputGroup className="mb-2">
+						<FormControl id="inlineFormInputGroup" placeholder="Должность" />
+					</InputGroup>
+				</Col>
+				<Col xs="auto">
+					<Form.Select className="mb-2">
+						<option>Поле #1</option>
+						<option>Поле #2</option>
+						<option>Поле #3</option>
+					</Form.Select>
+				</Col>
+				<Col xs="auto">
+					<Button type="submit" className="mb-2">
+						Подтвердить
+					</Button>
+				</Col>
+			</Row>
+		</Form>
+	);
 
 	return (
 		<>
@@ -20,7 +71,7 @@ const Fields = () => {
 						key={field.fieldNumber}
 						onClick={() => setSelectedField(field.fieldNumber)}
 						type="button"
-						className="btn btn-primary btn-lg m-1"
+						className="btn btn-primary btn-lg m-1 mt-4"
 					>
 						{field.fieldAdress}
 					</button>
@@ -28,7 +79,7 @@ const Fields = () => {
 				<button
 					onClick={reset}
 					type="button"
-					className="btn btn-danger btn-lg m-1"
+					className="btn btn-danger btn-lg m-1 mt-4"
 				>
 					Сброс
 				</button>
@@ -56,12 +107,13 @@ const Fields = () => {
 						</tbody>
 					</table>
 					<button
-						onClick={reset}
+						onClick={() => toggleUserFormActive()}
 						type="button"
-						className="btn btn-success btn-m m-1"
+						className="btn btn-success btn-m"
 					>
 						Добавить пользователя
 					</button>
+					{newUserForm ? renderNewUserForm() : ""}
 				</div>
 			) : (
 				""
